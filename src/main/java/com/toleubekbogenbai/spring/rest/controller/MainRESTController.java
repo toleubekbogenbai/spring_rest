23d9f0bbdece511c360a5ed41726283deb74ea51
toleu <toleubek.bogenbai@gmail.com>
2022-05-17 19:49:02 +0600

    Add  ControllerAdvice and add EmployeeGlobalExceptionHandler class

--- src/main/java/com/toleubekbogenbai/spring/rest/controller/MainRESTController.java
+++ src/main/java/com/toleubekbogenbai/spring/rest/controller/MainRESTController.java
@@ -30,20 +30,4 @@ public class MainRESTController {
         }
         return employee;
     }
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
-
-
 }
