db07b6aa82c2225ba4815437cfb9842f5d65d618
toleu <toleubek.bogenbai@gmail.com>
2022-05-17 19:00:40 +0600

    Add exceptions

---
+++ src/main/java/com/toleubekbogenbai/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.toleubekbogenbai.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message){
+        super(message);
+    }
+}
